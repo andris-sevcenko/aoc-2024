16fdb6947d56c29e8bb9e73f397bc9dd0095e9b8
Andris Ševčenko <andris@sevcenko.net>
2024-12-05 08:19:33 +0200

    Day 5

---
+++ 5/5.js
@@ -0,0 +1,73 @@
+import {getAllLines} from "../lib/utils.js";
+const startTime = performance.now()
+
+const isLess = {};
+const isMore = {};
+const manuals = [];
+
+let readingRules = true;
+for (const line of getAllLines('input.txt')) {
+    if (line.trim().length === 0) {
+        readingRules = false;
+        continue;
+    }
+
+    if (readingRules) {
+        const parts = line.split('|');
+        const less = parseInt(parts[0]);
+        const more = parseInt(parts[1]);
+
+        if (!isLess[less]) {
+            isLess[less] = [];
+        }
+        isLess[less].push(more);
+
+        if (!isMore[more]) {
+            isMore[more] = [];
+        }
+        isMore[more].push(less);
+    } else {
+        manuals.push(line)
+    }
+}
+
+const quickSort = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let pivot = arr[0];
+    let leftArr = [];
+    let rightArr = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        const num = arr[i];
+        if (isMore[pivot] && isMore[pivot].includes(num)) {
+            leftArr.push(arr[i]);
+        } else {
+            rightArr.push(arr[i]);
+        }
+    }
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+}
+
+let p1 = 0;
+let p2 = 0;
+
+for (const manual of manuals) {
+    if (quickSort(manual.split(',').map((el) => parseInt(el))).join(',') === manual) {
+        const parts = manual.split(',');
+        const start = Math.floor(parts.length / 2);
+        p1 += parseInt(parts.slice(start, start + 1));
+    } else {
+        const parts = quickSort(manual.split(',').map((el) => parseInt(el)))
+        const start = Math.floor(parts.length / 2);
+        p2 += parseInt(parts.slice(start, start + 1));
+    }
+}
+
+console.log('P1: ', p1);
+console.log('P2: ', p2);
+
+const endTime = performance.now()
+console.log(`Execution time in msecs: ${endTime - startTime}`)
