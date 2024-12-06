1547d947f56ed6a998aba5deabe53c03cf4c925e
Andris Ševčenko <andris@sevcenko.net>
2024-12-06 13:06:56 +0200

    Day 6.

--- 6/6.js
+++ 6/6.js
@@ -6,6 +6,7 @@ const grid = new Grid();
 class Guard {
     pos = [];
     dir = [];
+    visited = {};
 
     constructor(pos, dir) {
         this.pos = pos;
@@ -18,6 +19,13 @@ class Guard {
 
     move () {
         this.pos = this.getNext();
+        const hash = this.pos.join('.') + '-' + this.dir.join(',')
+
+        if (this.visited[hash]) {
+            return true;
+        }
+
+        this.visited[hash] = true;
     }
 
     turn () {
@@ -39,9 +47,11 @@ class Guard {
     }
 }
 
-grid.setGrid(getAllLines('input.txt'));
-const guard = new Guard([89, 51], [-1, 0])
-grid.setCell(89, 51, 'X');
+const inputGrid = getAllLines('input.txt')
+
+grid.setGrid(inputGrid);
+const startPos = [89, 51];
+const guard = new Guard(startPos, [-1, 0])
 
 do {
     const guardMove = guard.getNext();
@@ -62,7 +72,38 @@ do {
 const total = grid.count('X');
 console.log('P1: ', total);
 
+let p2 = 0;
+// Part 2 here we go
+for (let r = 0; r <= grid.maxGridRow; r++) {
+    for (let c = 0; c <= grid.maxGridCol; c++) {
+        grid.setGrid(inputGrid);
+        if (grid.getCell(r, c) === '#' || grid.getCell(r, c) === '^') {
+            continue;
+        }
+        grid.setCell(r, c, '#');
+        const p2Guard = new Guard(startPos, [-1, 0])
 
+        do {
+            const guardMove = p2Guard.getNext();
+            const nextMove = grid.getCell(guardMove[0], guardMove[1]);
+            if (!nextMove) {
+                break;
+            }
 
+            if (nextMove === '#') {
+                p2Guard.turn();
+                continue;
+            }
+
+            grid.setCell(guardMove[0], guardMove[1], 'X');
+            const res = p2Guard.move();
+            if (res) {
+                p2++;
+                break;
+            }
+        } while (true)
+    }
+}
+console.log('P2: ', p2);
 const endTime = performance.now()
 console.log(`Execution time in msecs: ${endTime - startTime}`)
